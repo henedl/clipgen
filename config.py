9ca81d1d67f92bbc0ecd70f8080ca9b846aa5cac
henedl <henrik@bigexplosion.se>
2026-04-03 18:21:29 +0200

    Bump version to 0.10.19, update color_matches tests for new return type

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ from icecream import ic
 REENCODING: bool = False
 AUDIO_NORMALIZE: bool = False
 FILEFORMAT: str = ".mp4"
-VERSIONNUM: str = "0.10.18"
+VERSIONNUM: str = "0.10.19"
 TITLECARDS_ENABLED: bool = (
     False  # use --titlecards / --no-titlecards to override per run
 )
